3677c072844f0fd6e88a7908a19618c03c072d15
yopi <yopiversi3@gmail.com>
2023-08-26 20:12:33 +0700

    profile

--- resources/views/admin/page/profile.blade.php
+++ resources/views/admin/page/profile.blade.php
@@ -8,11 +8,11 @@
                 <div class="text-center">
                     @if (isset($this_user) && $this_user->foto_user != '-')
                     <img class="profile-user-img img-fluid img-circle"
-                         src="{{asset('public/')}}/uploads/{{$this_user->foto_user}}"
+                         src="{{asset('public')}}/uploads/{{$this_user->foto_user}}"
                          alt="User profile picture">
                     @else
                     <img class="profile-user-img img-fluid img-circle"
-                         src="{{asset('public/')}}/img/user-default.png"
+                         src="{{asset('public')}}/img/user-default.png"
                          alt="User profile picture">
                     @endif
                     <br><br>
@@ -21,13 +21,13 @@
                             <div class="text-uppercase">
                                 <h4>{{$this_user->kode_invite}}</h4>
                             </div>
-                            
+
                         @endif
                         @if ($this_user->jabatan != '1')
                         <small>saldo anda</small>
                             <h4>Rp.{{number_format($this_user->saldo)}},-</h4>
                         @endif
-                   
+
                     @endif
 
                 </div>
@@ -38,18 +38,18 @@
                     <li class="list-group-item">
                         <b>Jabatan</b> <div class="float-right"> @switch($this_user->jabatan)
                             @case(0)
-                            <span class="badge badge-success">Administrator</span> 
+                            <span class="badge badge-success">Administrator</span>
                                 @break
                             @case(1)
-                            <span class="badge badge-success"> Owner</span> 
+                            <span class="badge badge-success"> Owner</span>
                                 @break
                             @case(2)
-                            <span class="badge badge-success"> Kasir</span>               
+                            <span class="badge badge-success"> Kasir</span>
                                 @break
                             @case(3)
-                            <span class="badge badge-success"> Teknisi</span> 
+                            <span class="badge badge-success"> Teknisi</span>
                                 @break
-                                
+
                             @endswitch</div>
                     </li>
                     <li class="list-group-item">
@@ -67,7 +67,7 @@
                             </li>
                         @endif
                     @endif
-                    
+
                 </ul>
                 <form action="{{route('signout')}}" method="post">
                     @csrf
@@ -84,9 +84,9 @@
                 <ul class="nav nav-pills">
                     <li class="nav-item"><a class="nav-link active" href="#profile" data-toggle="tab">Profile</a></li>
                     @if ($this_user->jabatan != '0')
-                        <li class="nav-item"><a class="nav-link" href="#gaji" data-toggle="tab">Penarikan Gaji</a></li>    
+                        <li class="nav-item"><a class="nav-link" href="#gaji" data-toggle="tab">Penarikan Gaji</a></li>
                     @endif
-                    
+
                   </ul>
             </div>
             <div class="card-body">
@@ -101,11 +101,11 @@
                                         <div class="col-md-4 text-center">
                                             @if (isset($this_user) && $this_user->foto_user != '-')
                                             <img class="profile-user-img img-fluid"
-                                                src="{{asset('public/')}}/uploads/{{$this_user->foto_user}}"
+                                                src="{{asset('public')}}/uploads/{{$this_user->foto_user}}"
                                                 alt="User profile picture" id="view-img">
                                             @else
                                             <img class="profile-user-img img-fluid"
-                                                src="{{asset('public/')}}/img/user-default.png"
+                                                src="{{asset('public')}}/img/user-default.png"
                                                 alt="User profile picture" id="view-img">
                                             @endif
                                         </div>
@@ -172,7 +172,7 @@
                                 <a href="#" class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_tarik_dana"><i class="fas fa-plus"></i> Tambah</a>
                                 <hr>
                             @endif
-                           
+
                             <div class="table responsive">
                                 <table class="table" id="TABLES_1">
                                     <thead>
@@ -201,7 +201,7 @@
                                                         <span class="badge badge-danger">Gagal</span>
                                                         @break
                                                     @default
-                                                        
+
                                                 @endswitch
                                             </td>
                                             <td>
@@ -210,10 +210,10 @@
                                                     @method('DELETE')
                                                     <a href="#" class="btn btn-info btn-sm" data-toggle="modal" data-target="#view_penarikan_{{$item->id_penarikan}}"><i class="fas fa-eye"></i></a>
                                                     @if ($this_user->jabatan == '1' && $item->status_penarikan == '0')
-                                                    <a href="#" class="btn btn-warning btn-sm"  data-toggle="modal" data-target="#edit_penarikan_{{$item->id_penarikan}}"><i class="fas fa-edit"></i></a>    
+                                                    <a href="#" class="btn btn-warning btn-sm"  data-toggle="modal" data-target="#edit_penarikan_{{$item->id_penarikan}}"><i class="fas fa-edit"></i></a>
                                                     @endif
                                                     @if ($item->status_penarikan == '0' && $this_user->jabatan == '1')
-                                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>    
+                                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
                                                     @endif
                                                 </form>
                                             </td>
@@ -258,7 +258,7 @@
                 <button type="submit" class="btn btn-success">Simpan</button>
             </div>
         </form>
-            
+
       </div>
       <!-- /.modal-content -->
     </div>
@@ -305,7 +305,7 @@
             <div class="modal-footer justify-content-between">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
             </div>
-            
+
       </div>
       <!-- /.modal-content -->
     </div>
@@ -350,7 +350,7 @@
                             <option value="2" {{$item->status_penarikan == '2' ? 'selected' : ''}}>Batal</option>
                         </select>
                     </div>
-                
+
                 @endif
             </div>
             <div class="modal-footer justify-content-between">
