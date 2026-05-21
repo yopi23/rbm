33281e60964336d4f5efea3293f342709cf3bee2
yopi23 <yopiversi3@gmail.com>
2026-05-22 06:52:59 +0700

    menangani error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,4 +47,58 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $e)
+    {
+        if ($request->is('api/*')) {
+            if ($e instanceof \Illuminate\Http\Exceptions\PostTooLargeException) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Ukuran file foto terlalu besar. Harap gunakan foto dengan ukuran lebih kecil.',
+                ], 413);
+            }
+
+            if ($e instanceof \Illuminate\Validation\ValidationException) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $e->getMessage(),
+                    'errors' => $e->errors(),
+                ], 422);
+            }
+
+            if ($e instanceof \Illuminate\Auth\AuthenticationException) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Unauthenticated.',
+                ], 401);
+            }
+
+            if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Endpoint tidak ditemukan.',
+                ], 404);
+            }
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Terjadi kesalahan server: ' . $e->getMessage(),
+            ], $this->isHttpException($e) ? $e->getStatusCode() : 500);
+        }
+
+        if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+            return redirect('/');
+        }
+
+        return parent::render($request, $e);
+    }
 }
